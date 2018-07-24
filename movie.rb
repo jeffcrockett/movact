45a6baf850f435af29b81e4968928355d8754a10
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-07-23 22:42:01 -0400

    Cleaned up movie.rb

--- movie.rb
+++ movie.rb
@@ -18,21 +18,13 @@ class Actor
         Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{@name}_filmography"))
     end
         #movie url
-    def get_movie_url
-        self.get_page.css('#mw-content-text > div > table:nth-child(8) > tbody').children[2].children[3].children.children[0].attributes['href'].value
-    end
 
     def movies
         self.get_page.css('#mw-content-text > div > table > tbody tr td i a').map do |item|
             item.attributes['href'].value    
         end
         # binding.pry
-        #todo: iterate over all children, not just (2...10)
-        # (2..10).step(2).map do |index|
-        #     movie_table[index].children[3].children.children[0].attributes['href'].value
-            # binding.pry
-        # movie_table[2].children[3].children.children[0].attributes['title'].value
-        # self.get_page(name).css('#mw-content-text > div > table:nth-child(8) > tbody').children[2].children[3].children.children[0].attributes['title'].value
+ 
     end
 end
 binding.pry
